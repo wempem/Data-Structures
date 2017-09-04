d20a523c98c2560f2cadbd96743f0b79e17cefc5
Dr. West <pwest@cs.univ.edu>
2017-09-04 06:39:54 -0400

    lab04 update

---
+++ lab04/src/arrays.cpp
@@ -0,0 +1,11 @@
+int countNegatives(const int array[], int size) {
+    return 0;
+}
+
+int findMinOdd(const int array[], int size) {
+    return 1;
+}
+
+int search(const int array[], int size, int value) {
+    return 0;
+}
---
+++ lab04/test/min-odd-test.cpp
@@ -0,0 +1,19 @@
+#include <cxxtest/TestSuite.h>
+#include <limits.h>
+#include <stdio.h>
+#include "arrays.hpp"
+
+class MyTestSuite1 : public CxxTest::TestSuite {
+    public:
+        void testFirst(void) {
+            const int iarray[] = {0, 1, 2, 3, 4, 5};
+            TS_ASSERT(findMinOdd(iarray, 6) == 1);
+            /* Fill in some test cases here for cxx test gen */
+        }
+        void testSecond(void) {
+            const int iarray[] = {0, 11, 2, 3, 4, 5};
+            TS_ASSERT(findMinOdd(iarray, 6) == 3);
+            /* Fill in some test cases here for cxx test gen */
+        }
+};
+
