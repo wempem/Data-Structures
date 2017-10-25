23e16e354d98cd9f3727100a97632b41c31d79bc
Dr. West <pwest@cs.univ.edu>
2017-10-25 07:12:03 -0400

    lab16

---
+++ lab16/src/heap.cpp
@@ -0,0 +1,22 @@
+#ifdef HEAP_H
+
+template<class T>
+Heap<T>::Heap(const int len) {
+}
+
+template<class T>
+void Heap<T>::insert(const T& element) {
+}
+
+template<class T>
+T& Heap<T>::operator[](const int pos) {
+}
+
+template<class T>
+T* Heap<T>::heapSort() {
+}
+
+template<class T>
+Heap<T>::~Heap() {
+}
+#endif
---
+++ lab16/src/main.cpp
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include "heap.hpp"
+
+int main(int argc, char *argv[]) {
+    return 0;
+}
